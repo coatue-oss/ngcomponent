2f919dd595d631f76e24c52df68f2458ef84c5e0
Chris Khoo <khoomeister@users.noreply.github.com>
2017-04-12 14:16:32 -0700

    Revert "implement setState"

--- index.ts
+++ index.ts
@@ -3,11 +3,9 @@ import {assign, mapValues, some} from 'lodash'
 abstract class NgComponent<Props, State> {
 
   private __isFirstRender = true
-  private __nextState: State
-  private __setStateTriggersLifecycle = true
 
-  props: Props = {} as Props
   protected state: State = {} as State
+  public props: Props = {} as Props
 
   /*
     eg. {
@@ -16,35 +14,23 @@ abstract class NgComponent<Props, State> {
     }
   */
   // nb: this method is explicity exposed for unit testing
-  $onChanges(changes: object) {
+  public $onChanges(changes: object) {
     const oldProps = mapValues<{}, Props>(changes, 'previousValue')
     const newProps = mapValues<{}, Props>(changes, 'currentValue')
-    if (!this.__isFirstRender && !this.didPropsChange(newProps, oldProps)) return
-    this.lifecycle(newProps, {} as State)
-  }
 
-  // note: this isn't meant to exactly replicate React's setState (e.g. http://stackoverflow.com/q/28922275/7816712)
-  protected setState(newState: Partial<State>) {
-    if (this.__setStateTriggersLifecycle) {
-      setTimeout(() => this.lifecycle({} as Props, newState), 0)
-    } else {
-      this.__nextState = assign({}, this.state, newState)
-    }
-  }
-
-  private lifecycle(newProps: Partial<Props>, newState: Partial<State>) {
     const nextProps = assign({}, this.props, newProps)
-    this.__nextState = assign({}, this.state, newState)
+    // TODO: implement nextState (which also means implement this.setState)
 
     if (this.__isFirstRender) {
-      assign(this, { props: nextProps, state: this.__nextState })
-      this.avoidSetStateLifecycle(() => this.componentWillMount())
+      assign(this, { props: nextProps })
+      this.componentWillMount()
       this.render()
       this.__isFirstRender = false
     } else {
-      this.avoidSetStateLifecycle(() => this.componentWillReceiveProps(nextProps))
-      const shouldUpdate = this.shouldComponentUpdate(nextProps, this.__nextState)
-      assign(this, { props: nextProps, state: this.__nextState })
+      if (!this.didPropsChange(newProps, oldProps)) return
+      this.componentWillReceiveProps(nextProps)
+      const shouldUpdate = this.shouldComponentUpdate(nextProps, this.state)
+      assign(this, { props: nextProps })
       if (!shouldUpdate) return
 
       this.componentWillUpdate(this.props, this.state)
@@ -61,14 +47,8 @@ abstract class NgComponent<Props, State> {
     this.componentWillUnmount()
   }
 
-  private didPropsChange(newProps: Props, oldProps: Props): boolean {
+  protected didPropsChange(newProps: Props, oldProps: Props): boolean {
     return some(newProps, (v, k) => v !== oldProps[k])
-  }
-
-  private avoidSetStateLifecycle(fn: () => void) {
-    this.__setStateTriggersLifecycle = false
-    fn()
-    this.__setStateTriggersLifecycle = true
   }
 
   /*
@@ -76,7 +56,7 @@ abstract class NgComponent<Props, State> {
   */
   componentWillMount(): void {}
   componentDidMount(): void {}
-  componentWillReceiveProps(props: Props): void {}
+  componentWillReceiveProps(props: Props): void { }
   shouldComponentUpdate(nextProps: Props, nextState: State): boolean { return true }
   componentWillUpdate(props: Props, state: State): void {}
   componentDidUpdate(props: Props, state: State): void {}
